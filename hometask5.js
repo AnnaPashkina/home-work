85d33e253c781ea0d2797190f6f872502051682d
AnnaPashkina <119527969+AnnaPashkina@users.noreply.github.com>
2023-12-02 16:22:45 +0200

    Add files via upload

---
+++ hometask5.js
@@ -0,0 +1,46 @@
+
+//let a = 'км';
+//let a = 'год';
+/*let a = 'кг';
+let b = 10000;
+let b1 = 60;
+let b2 = 1000
+switch (a) {
+ case 'км':
+ console.log(10 + a + " це " + b + " м");
+ break;
+   case 'год':
+    console.log(1 + a + " це " + b1 + " хв");
+    break;
+       case 'кг':
+       console.log(1 + a + " це "+ b2 + " грм");
+    break;
+    default:
+        console.log('Що ???');
+        
+}*/
+
+//Якщо буде введена нова одиниця код матиме наступний вигляд:
+
+//let a = 'км';
+//let a = 'год';
+//let a = 'кг';
+let a = 'л'
+let b = 10000;
+let b1 = 60;
+let b2 = 1000
+switch (a) {
+ case 'км':
+ console.log(10 + a + " це " + b + " м");
+ break;
+   case 'год':
+    console.log(1 + a + " це " + b1 + " хв");
+    break;
+       case 'кг':
+       console.log(1 + a + " це "+ b2 + " грм");
+       case 'л':
+       console.log(1 + a + " це "+ b2 + " мл")
+    break;
+    default:
+        console.log('Невірне значення');        
+}
