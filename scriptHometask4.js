36134d7fcc52cf8fe76109c2a6427d632b2083f8
AnnaPashkina <119527969+AnnaPashkina@users.noreply.github.com>
2023-12-02 17:24:12 +0200

    Add files via upload

--- scriptHometask4.js
+++ scriptHometask4.js
@@ -8,7 +8,9 @@
     } else if (age > 4 && age < 20 || age > 24 && age < 31 || age > 34 && age < 41 || age > 44 && age < 51 || age > 54 && age < 61
         || age > 64 && age < 71 || age > 74 && age < 81 || age > 84 && age < 91 || age > 94 && age < 101)  {
         console.log (" Мені " + age + " років.")
-    } else if (age < 0) {console.log ("Вік не може мати від'ємне значення")}
+    }               else if (age == ("") ) {console.log ("Заповни поле")}
+    else if (age == (0) ) {console.log ("ТОбі 0 років?")}
+      else if (age < 0) {console.log ("Вік не може мати від'ємне значення")}
             else {
             console.log(" Мені " + age + " рік")
             }
