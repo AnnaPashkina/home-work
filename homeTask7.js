774621a675f0d2666bea0813f86372804d50fe66
AnnaPashkina <anya230489@gmail.com>
2023-12-30 22:09:57 +0200

    homeTask7

---
+++ homeTask7.js
@@ -0,0 +1,51 @@
+//Завдання 1. 
+/*let a = 'qwerty' / 10;
+//let a = 7 * 10
+if (a <0 || a > 0) 
+{console.log('це число')
+}
+else {console.log('це не число')
+}*/
+
+//Завдання 2
+ var a = 'qwerty';
+  var simbol = '';
+  //var bool = true
+  var bool = false
+   for (var amount = 0; amount < 4; amount++)
+  {simbol += "+"; }
+  if (bool == true)
+    {console.log (simbol + a)}
+    else if (bool == false)
+    {console.log (a + simbol)}
+    else
+      {console.log ("Помилка")}
+
+     
+     
+// Завдання 3
+/*let rnd = Math.random();
+rnd *= 1000;
+rnd = Math.round(rnd)
+let sum = 0 // Кількіссть згенерованих чисел
+let sum1 = 0 // Кількість парних чисел в згенерованому наборі випадкових чисел
+let sum2 = 0 // Кількість непарних чисел в згенерованому наборі випадкових чисел
+for ( let amount = 0;   amount < 21; amount++) 
+{ rnd += amount
+sum += rnd/rnd
+if (rnd % 2 ==0)
+sum1 += rnd/rnd
+if (rnd % 2 == 1)
+sum2 += rnd/rnd}
+console.log("Кількість згенерованих чисел дорівнює " + sum)
+console.log("Кількість парних чисел дорівнює " + sum1)
+console.log("Кількість непарних чисел дорівнює " + sum2)
+console.log("Прицентний вміст непарних чисел =" + (sum2/sum)*100, "%")
+console.log("Прицентний вміст парних чисел " +(sum1/sum)*100, " % ")
+console.log("Співвідношення парних чисел до непрних " + (sum1/sum)*100, "%", (sum2/sum)*100, "%")*/
+
+
+
+
+
+ 
