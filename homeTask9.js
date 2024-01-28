686162d5afd04046569eac0608b13c3c2745eb25
AnnaPashkina <anya230489@gmail.com>
2024-01-28 23:48:32 +0200

    commit1

---
+++ homeTask9.js
@@ -0,0 +1,55 @@
+//"use strict"
+
+//Завдання 1
+
+/*function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+shuffle(arr);
+console.log(arr)*/
+
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків'}
+    let clients = [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+           partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+    console.log(partners[0])
+
+console.log(clients[1])
